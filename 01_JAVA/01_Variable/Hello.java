f99a5b3af3fa1e30f5a31b76844afc0d95c8b8a6
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-11-14 21:56:37 +0900

    191101 이름 표기법

---
+++ 01_JAVA/01_Variable/Hello.java
@@ -0,0 +1,21 @@
+
+public class Hello {
+//	클래스 이름의 생성규칙 : 파스칼 표기법(Pascal Case)
+//	이 역시 단어의 첫 글자를 대문자로 사용하는 것은 카멜 표기법과 같지만 연달아 오는 단어의 모든 앞글자를 대문자로 표기한다.
+//	1. 클래스의 첫 글자는 대문자. ex) Hello
+//	2. 여러단어로 이루어진 경우 첫글자를 대문자로. ex) StringBuffer
+	
+//	메소드/변수 이름 표기법 : 카멜 표기법(Camel Case)
+//	여러 단어를 연달아 사용할 때 각 단어의 첫 글자를 대문자로 적되, 맨 앞에 오는 글자는 소문자로 표기하는 것이다.
+//	ex) mainMethod
+//	ex) int totalScore;
+	
+//	상수 : 프로그램 내에서 항상 동일한 값을 가지는 변수.
+//	모두 다 대문자. 
+//	ex) PI  
+//	ex) MAX_NUMBER
+	
+	public static void main(String []args) {
+		System.out.println("Hello, world.");
+	} //main end
+} //class end
