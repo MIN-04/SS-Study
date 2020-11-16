1f3ca66633809296acd79ae859579ea4bcd73683
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-11-16 21:16:26 +0900

    191104 [리터럴] 이스케이프 시퀀스 예제 02

---
+++ 01_JAVA/02_Literal/Variable_Literal_Ex02.java
@@ -0,0 +1,17 @@
+package kr.co.sist01.literal;
+
+public class Variable_Literal_Ex02 {
+
+	public static void main(String[] args) {
+		//2. 문자와 숫자 등을 사용하여 다음과 같이 화면에 출력하는 코드를 작성 하세요.
+//		456
+//		\ 88을 받았다.
+//		좋은 하루 되세요.
+		
+		System.out.println("456");
+		System.out.println("\\ 88을 받았다.");
+		System.out.println("좋은 하루 되세요.");
+
+	}
+
+}
