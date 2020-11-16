15fe36cda4e0a0c5b85357e1650c7c8b3a481ef5
Kim MJ <62365297+MIN-04@users.noreply.github.com>
2020-11-16 21:15:42 +0900

    191104 [리터럴] 이스케이프 시퀀스 예제

---
+++ 01_JAVA/02_Literal/Variable_Literal_Ex01.java
@@ -0,0 +1,12 @@
+package kr.co.sist01.literal;
+
+public class Variable_Literal_Ex01 {
+
+	public static void main(String[] args) {
+		//1. Tab기호를 표현하는 이스케이프 시퀀스를 사용하여 다음과 같이 출력 하세요.
+		// 8 10 13
+		System.out.println("8\t10\t13"); //8	10	13
+
+	}
+
+}
